73c70bd706a55e3bf08acc942d3fb0a9f1cce8e1
Oleg Gnatyk <103590840+yavenir@users.noreply.github.com>
2022-07-12 13:30:27 +0300

    Completed 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    buttonStart.setAttribute('disabled', 'disabled');
+    console.log('in progress...');
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.removeAttribute('disabled');
+  console.log('stop progress');
+  clearInterval(timerId);
+});
