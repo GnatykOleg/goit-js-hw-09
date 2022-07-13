b87572d8a7ab744d46672c28ca13e93c0397d99f
Oleg Gnatyk <103590840+yavenir@users.noreply.github.com>
2022-07-13 21:25:16 +0300

    Complete task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', onFormClick);
+
+function onFormClick(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let delayEl = Number(delay.value);
+  let stepEl = Number(step.value);
+  let amountEl = Number(amount.value);
+
+  console.log(delayEl, stepEl, amountEl);
+
+  for (let i = 1; i <= amountEl; i += 1) {
+    createPromise(i, stepEl * i)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    //  stepEl * i;
+    // delayEl += stepEl;
+  }
+
+  event.currentTarget.reset();
+}
+
+// Перед першим промісом буде одна затримка delayEl
+// Між іншими промісами затримка stepEl
