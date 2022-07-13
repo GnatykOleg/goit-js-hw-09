76778112be0cf6c4e7b5cb0fe7d4ec7b0b878a71
Oleg Gnatyk <103590840+yavenir@users.noreply.github.com>
2022-07-13 15:17:22 +0300

    Completed task number 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,35 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const timerContainer = document.querySelector('.timer');
-const timerField = document.querySelectorAll('.field');
-const timerSpanValue = document.querySelectorAll('.value');
-const timerSpanLabel = document.querySelectorAll('.label');
-const buttonStart = document.querySelector('[data-start]');
 const inputDateTimePicker = document.querySelector('#datetime-picker');
+const buttonStart = document.querySelector('[data-start]');
+let dataDays = document.querySelector('[data-days]');
+let dataHours = document.querySelector('[data-hours]');
+let dataMinutes = document.querySelector('[data-minutes]');
+let dataSeconds = document.querySelector('[data-seconds]');
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+let timerId = null;
+
+buttonStart.disabled = true;
+
+let targetDate = null;
+
+const addLeadingZero = value => String(value).padStart(2, 0);
 
 const options = {
   enableTime: true,
@@ -14,31 +37,37 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    console.log('ЭТО МАССИВ ВЫБРАННЫХ ДАТ:', selectedDates[0]);
+    // console.log('РАЗНИЦА', convertMs(selectedDates[0] - options.defaultDate));
+
+    if (Date.now() > selectedDates[0]) {
+      buttonStart.disabled = true;
+      Notify.failure('Please choose a date in the future', { timeout: 2000 });
+      return;
+    }
+    buttonStart.disabled = false;
+    targetDate = selectedDates[0];
   },
 };
 
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  inputDateTimePicker.disabled = true;
+
+  timerId = setInterval(() => {
+    const delta = targetDate - Date.now();
+    const data = convertMs(delta);
+    console.log(data);
+
+    dataDays.textContent = addLeadingZero(data.days);
+    dataHours.textContent = addLeadingZero(data.hours);
+    dataMinutes.textContent = addLeadingZero(data.minutes);
+    dataSeconds.textContent = addLeadingZero(data.seconds);
+
+    if (delta <= 1000) {
+      clearInterval(timerId);
+    }
+  }, 1000);
+});
+
 flatpickr(inputDateTimePicker, options);
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
